5a1a664bc325557f4da9674c2c0032f94ba6ea27
John Dance <guitar.hero.john@gmail.com>
2023-01-02 19:03:38 -0500

    commit tree d76b5f7dd5301d94f2f559b9b2ee8e4e7592d1bc

---
+++ index.js
@@ -0,0 +1,14 @@
+function shout(string) {
+    // todo
+  }
+  function shout(string) {
+    return string.toUpperCase();
+  }
+function whisper(string) {
+}
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+function logShout(string) {
+}
+  console.log(logShout);
